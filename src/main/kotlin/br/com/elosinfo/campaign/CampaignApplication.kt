9998fa362e0b05905c32a94f0e3718ec26be4b4a
Eduardo Souza <eduardo.souza@ifood.com.br>
2021-09-29 21:36:58 -0300

    Configurações iniciais do projeto

--- src/main/kotlin/br/com/elosinfo/campaign/CampaignApplication.kt
+++ src/main/kotlin/br/com/elosinfo/campaign/CampaignApplication.kt
@@ -1,10 +1,17 @@
 package br.com.elosinfo.campaign
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class CampaignApplication
+class CampaignApplication : SpringBootServletInitializer(){
+
+	protected override fun configure(application: SpringApplicationBuilder) : SpringApplicationBuilder {
+		return application.sources(CampaignApplication::class.java)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<CampaignApplication>(*args)
---
+++ src/main/kotlin/br/com/elosinfo/campaign/web/controller/CampaignController.kt
@@ -0,0 +1,16 @@
+package br.com.elosinfo.campaign.web.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/campaigns")
+class CampaignController {
+    @GetMapping("/")
+    fun hello(): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.OK).body("Olá mundo REST!")
+    }
+}
